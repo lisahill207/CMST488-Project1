31a69dbbd3ad48b58fcc24aa8d81cbcf7e080af2
Lisa Hill <lisahill207@gmail.com>
2023-10-28 18:43:48 -0400

    server fuctioning

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 // ✅ TODO: Initialize variables (set port variable, and import http, httpStatus, fs, path modules)
-const port = 8000,
-  http = require('http'),
+const port = 8000;
+const http = require('http'),
   httpStatus = require('http-status'),
   fs = require('fs'),
-  path = require('path')
-  const app = express();
+  path = require('path');
 
-const routeMap = {
-  "/": "views/index.html"
-};
+const express = require('express'),
+  app = express();
+
 // Import resources for API
 const resources = require("./models/resources");
 
 // Create error handling / response
-const sendErrorResponse = (req, res) => {
-  res.writeHead(httpStatus.NOT_FOUND, {
-    "Content-Type": "text/html",
-  });
-  // ✅ TODO: Implement res.end with error message in h1 tags with text "Resource not found"
-  res.end("<h1>Resource not found</h1>");
+sendErrorResponse = (res) => {
+  res.writeHead(404, {"Content-Type": "text/html"});
+// ✅ TODO: Implement res.end with error message in h1 tags with text "Resource not found"
+  res.write("<h1>Resource not found</h1>");
+  res.end();
 };
 
 // Create Web Server
-const server = http.createServer(function (req, res) {
-  // Implement healthcheck URL at /healthcheck
+const server = http.createServer((req, res) => {
+    res.writeHead(httpStatus.OK, {
+      "Content-Type": "text/html"
+    });
+// Implement healthcheck URL at /healthcheck
   if (req.url === "/healthcheck") {
-        // ✅TODO: Implement healthcheck code here
-    app.use('/healthcheck', require('./routes/healthchecker'));
+// ❓TODO: Implement healthcheck code here
+      app.use('/healthcheck', require('./healthchecker.js'));
   }
-
   // Implement static file system and serve /views/index.html
   // ** OPTIONAL: Setup dynamic reading and serving of other static files (Hint: see lesson 6.1 Wexxler)
-  else if (req.url === "/views/index.html") {
+  else if (req.url === "/") {
     fs.readFile(path.join(__dirname, "views", "index.html"), (error, data) => {
-      if (error) {
-        sendErrorResponse(res);
-      }
       // ✅TODO: Implement res.writehead to send header information - 200 response content type html
-    let responseMessage = "<h1>Hello, Universe!</h1>";
-      res.writehead(responseMessage);
+      res.writeHead(200, 'Content-Type": "text/html');
+      res.write('<h1>Hello, Universe!</h1>');
       // ✅TODO: Implement res.end to send data
-    res.end();
-      console.log(`Sent a response : ${responseMessage}`);
+      res.end(data);
     });
-  }
-
-  // Add a basic api to serve resources.js
-  else if (req.url == "/api/resources") {
-    // ❓TODO: Implement res.writeHead to send httpStatus.OK with JSON content type
-    // ❓TODO: Implement res.end and use JSON.stringify to return resources
   } else {
     sendErrorResponse(res);
   }
+
+  // Add a basic api to serve resources.js
+  /*else if (req.url == "/api/resources") {
+    // ❓TODO: Implement res.writeHead to send httpStatus.OK with JSON content type
+    res.writeHead(200, 'Content-Type": "application/json')
+    // ❓TODO: Implement res.end and use JSON.stringify to return resources
+    res.end(JSON.stringify(resources.js))
+  }else {
+    sendErrorResponse(res);
+  }*/
 });
-
 server.listen(port); // listen for any incoming requests;
-
 console.log(`The server has started and is listening on port number: ${port}`);
