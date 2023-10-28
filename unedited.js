d27e019e8b39f3b08abdec12b831f1cf4be79455
Lisa Hill <lisahill207@gmail.com>
2023-10-28 10:18:53 -0400

    starting saturday'

--- unedited.js
+++ unedited.js
@@ -1,12 +1,5 @@
-// ✅ TODO: Initialize variables (set port variable, and import http, httpStatus, fs, path modules)
-const port = 8000,
-  http = require("http"),
-  httpStatus = require("http-status-codes"),
-  fs = require(fs);
+// TODO: Initialize variables (set port variable, and import http, httpStatus, fs, path modules)
 
-const routeMap = {
-  "/": "views/index.html"
-};
 // Import resources for API
 const resources = require("./models/resources");
 
@@ -15,15 +8,14 @@ const sendErrorResponse = (req, res) => {
   res.writeHead(httpStatus.NOT_FOUND, {
     "Content-Type": "text/html",
   });
-  // ✅ TODO: Implement res.end with error message in h1 tags with text "Resource not found"
-  res.end("<h1>Resource not found</h1>");
+  // TODO: Implement res.end with error message in h1 tags with text "Resource not found"
 };
 
 // Create Web Server
 const server = http.createServer(function (req, res) {
   // Implement healthcheck URL at /healthcheck
   if (req.url === "/healthcheck") {
-    // ❓TODO: Implement healthcheck code here
+    // TODO: Implement healthcheck code here
   }
 
   // Implement static file system and serve /views/index.html
@@ -33,19 +25,15 @@ const server = http.createServer(function (req, res) {
       if (error) {
         sendErrorResponse(res);
       }
-      // ✅TODO: Implement res.writehead to send header information - 200 response content type html
-    let responseMessage = "<h1>Hello, Universe!</h1>";
-      res.writehead(responseMessage);
-      // ✅TODO: Implement res.end to send data
-    res.end();
-      console.log(`Sent a response : ${responseMessage}`);
+      // TODO: Implement res.writehead to send header information - 200 response content type html
+      // TODO: Implement res.end to send data
     });
   }
 
   // Add a basic api to serve resources.js
   else if (req.url == "/api/resources") {
-    // ❓TODO: Implement res.writeHead to send httpStatus.OK with JSON content type
-    // ❓TODO: Implement res.end and use JSON.stringify to return resources
+    // TODO: Implement res.writeHead to send httpStatus.OK with JSON content type
+    // TODO: Implement res.end and use JSON.stringify to return resources
   } else {
     sendErrorResponse(res);
   }
