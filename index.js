922425b12acb4e657ff9cf65aafdf1f945d265ac
Lisa Hill <lisahill207@gmail.com>
2023-11-08 15:15:55 -0500

    fixed initial server issues

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 8000,
 const resources = require("./models/resources");
 
 // Create error handling / response
-sendErrorResponse = (res) => {
+const sendErrorResponse = (res) => {
   res.writeHead(404, {"Content-Type": "text/html"});
 // ✅ TODO: Implement res.end with error message in h1 tags with text "Resource not found"
   res.write("<h1>Resource not found</h1>");
@@ -22,19 +22,23 @@ const server = http.createServer((req, res) => {
     res.writeHead(httpStatus.OK, {
       "Content-Type": "text/html"
     });
+    if (req.url === "/"){
+    let responseMessage = "<h1>Welcome!</h1>";
+    res.end(responseMessage);
+    }
 // Implement healthcheck URL at /healthcheck
-  if (req.url === "/healthcheck") {
+  else if (req.url === "/healthcheck") {
 // ✅TODO: Implement healthcheck code here
       const uptime = process.uptime();
       res.write(`Your uptime is ${uptime} seconds`);
     }
   // Implement static file system and serve /views/index.html
   // ** OPTIONAL: Setup dynamic reading and serving of other static files (Hint: see lesson 6.1 Wexxler)
-  else if (req.url === "/") {
+  else if (req.url === "/views/index") {
     fs.readFile(path.join(__dirname, "views", "index.html"), (error, data) => {
       // ✅TODO: Implement res.writehead to send header information - 200 response content type html
       res.writeHead(200, 'Content-Type": "text/html');
-      res.write('<h1>Hello, Universe!</h1>');
+      //res.write('<h1>Hello, Universe!</h1>');
       // ✅TODO: Implement res.end to send data
       res.end(data);
     });
@@ -45,7 +49,8 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, 'Content-Type": "application/json')
     // ✅TODO: Implement res.end and use JSON.stringify to return resources
     res.end(JSON.stringify(resources));
-  }else {
+  }
+  else {
     sendErrorResponse(res);
   }
 });
